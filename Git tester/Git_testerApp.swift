921c369f6198a755c19b09f0ce36b61ea0270682
Rishab Sudhir <rishabsudhir2002@gmail.com>
2023-01-06 20:05:25 +0400

    Initial Commit

---
+++ Git tester/Git_testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_testerApp.swift
+//  Git tester
+//
+//  Created by Rishab Sudhir on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
