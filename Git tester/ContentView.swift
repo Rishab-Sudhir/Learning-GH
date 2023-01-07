0c1742e123db2a88f8de8d43b32f0e105fd25691
Rishab Sudhir <rishabsudhir2002@gmail.com>
2023-01-07 18:51:45 +0400

    add image

--- Git tester/ContentView.swift
+++ Git tester/ContentView.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("wasssaaap!")
-        }
+            VStack{
+                Image(systemName: "figure.wave")
+                Text("wasssaaap!")
+            }
         .padding()
     }
 }
